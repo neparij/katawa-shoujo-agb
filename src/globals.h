ddcee2c4c9e47d85830b700df3d067d84c1a3620
neparij <neparij@mail.ru>
2026-01-22 01:48:54 +0400

    Add disabled text menu palette

--- src/globals.h
+++ src/globals.h
@@ -9,6 +9,8 @@
 #include <bn_sprite_items_fontpalette_beige_bold.h>
 #include <bn_sprite_items_fontpalette_beige_selected.h>
 #include <bn_sprite_items_fontpalette_beige_selected_bold.h>
+#include <bn_sprite_items_fontpalette_beige_disabled.h>
+#include <bn_sprite_items_fontpalette_beige_disabled_bold.h>
 #include <bn_sprite_palette_item.h>
 #include <bn_unique_ptr.h>
 
@@ -95,6 +97,7 @@ namespace ks::globals {
         const bn::sprite_palette_item original = bn::sprite_items::fontpalette_main.palette_item();
         const bn::sprite_palette_item beige = bn::sprite_items::fontpalette_beige.palette_item();
         const bn::sprite_palette_item beige_selected = bn::sprite_items::fontpalette_beige_selected.palette_item();
+        const bn::sprite_palette_item beige_disabled = bn::sprite_items::fontpalette_beige_disabled.palette_item();
 
         [[nodiscard]] inline bn::sprite_palette_item bold(const bn::sprite_palette_item& text_item_palette) {
             if (text_item_palette == original) {
@@ -105,6 +108,9 @@ namespace ks::globals {
             }
             if (text_item_palette == beige_selected) {
                 return bn::sprite_items::fontpalette_beige_selected_bold.palette_item();
+            }
+            if (text_item_palette == beige_disabled) {
+                return bn::sprite_items::fontpalette_beige_disabled_bold.palette_item();
             }
 
             BN_ERROR("Unknown palette type for bold variant");
